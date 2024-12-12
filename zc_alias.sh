a19c86be8599e7dbe05dbbe3cce7d24fac2356fa
ziancn <87293750+ziancn@users.noreply.github.com>
2024-12-12 14:54:50 +0800

    Create zc_alias.sh

---
+++ zc_alias.sh
@@ -0,0 +1,15 @@
+# Start of zcalias
+
+# Faster ls
+alias l="ls -l"
+alias lt="ls -ltr"
+
+# Grep a CSV with table head
+function csvgrep() {
+    local pattern="$1"
+    local file="$2"
+    {
+        sed 's/\r$//; s/\r/\n/g' "$file" | head -1
+        sed 's/\r$//; s/\r/\n/g' "$file" | grep "$pattern"
+    } | column -ts,
+}
